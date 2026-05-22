13b42aa46977253c455fdac2cb803428a350171d
test5tiempo <test5@tiempo.hn>
2026-05-22 16:04:06 -0600

    Restore webview import for article detail parts

--- lib/screens/noticias/noticias.screen.dart
+++ lib/screens/noticias/noticias.screen.dart
@@ -14,6 +14,8 @@ import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 part 'categoria_noticias.screen.dart';
 part 'noticia_detalle.screen.dart';
